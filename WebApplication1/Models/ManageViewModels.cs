294962adeb05ae02b3aab92e8da104de41a633bb
NielsTyler <dvtsvetkov@gmail.com>
2017-12-15 17:29:21 +0300

    Test change

--- WebApplication1/Models/ManageViewModels.cs
+++ WebApplication1/Models/ManageViewModels.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
 namespace WebApplication1.Models
-{
+{//test
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
